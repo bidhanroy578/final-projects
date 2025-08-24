0d073dea79303f18ec1ad17fde0e53e99a6731ad
bidhan <bidhanroy578@gmail.com>
2025-08-24 21:28:49 +0600

    fix: Update CORS configuration to allow requests from production URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://bistro-boss-broy.web.app"],
     credentials: true,
   })
 );
